8d3483895a47ba8a7b33c9413b9e8979292c2051
LAPTOP-O404DRVG\EX <cyndy99lin@gmail.com>
2024-05-06 22:42:58 +0800

    character animation

--- HopperExpress/Assets/scripts/Characters/Skills.cs
+++ HopperExpress/Assets/scripts/Characters/Skills.cs
@@ -7,7 +7,7 @@ public class Skills : MonoBehaviour
     public static bool skill_floorspike=false;
     public static bool skill_forcefield=false;
     public static bool skill_throwfire=false;
-    public static bool skill_throwice=true;
+    public static bool skill_throwice=false;
     public static bool skill_electricfance=false;
 
     // Start is called before the first frame update
---
+++ HopperExpress/Assets/scripts/abilities/IceBomb/IceFog.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceFog : MonoBehaviour
+{
+    public float destroyAfterSec = 5f;
+    public float slowDownTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,destroyAfterSec);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<MonsterManager>(out MonsterManager enemyComponent))
+        {
+            enemyComponent.ColdSlow(slowDownTime);
+
+        }
+    }
+}
